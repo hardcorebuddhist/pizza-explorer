7be59c4fb6686d7996caf8abdcf21966f118839e
hardcorebuddhist <im.energo@gmail.com>
2022-05-10 14:58:09 +0200

    logic for toggleFavorites button

--- src/components/PizzaList.jsx
+++ src/components/PizzaList.jsx
@@ -1,22 +1,30 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { selectUser } from "../store/user/selectors";
 import { selectPizzas } from "../store/pizza/selectors";
+import { toggleFavorites } from "../store/user/slice";
 
 export default function PizzaList() {
   const user = useSelector(selectUser);
   const pizzas = useSelector(selectPizzas);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h1>Pizza Explorer</h1>
       <p>
-        Welcome back, <strong>{user.name}</strong>! There are
+        Welcome back, <strong>{user.name}</strong>! There are{" "}
         <strong>{pizzas.length}</strong> pizzas in total.
       </p>
       <p>TODO: the list of pizzas</p>
       <ul>
         {pizzas.map((pizza) => (
           <li key={pizza.id}>
-            <h2>{pizza.name}</h2>
+            <h2>
+              {pizza.name}{" "}
+              <button onClick={() => dispatch(toggleFavorites(pizza.id))}>
+                {user.favorites.includes(pizza.id) ? "♥" : "♡"}
+              </button>
+            </h2>
             <div>
               <img
                 src={pizza.image}
--- src/store/user/slice.js
+++ src/store/user/slice.js
@@ -1,17 +1,26 @@
-// src/store/user/slice.js
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   name: "Helva",
   id: 42,
+  favorites: [357311, 67283],
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    toggleFavorites: (state, action) => {
+      const idToAdd = action.payload;
+      const newFavs = state.favorites.includes(idToAdd)
+        ? state.favorites.filter((idNumber) => idNumber !== idToAdd)
+        : [...state.favorites, idToAdd];
+
+      state.favorites = newFavs;
+    },
+  },
 });
 
-export const {} = userSlice.actions;
+export const { toggleFavorites } = userSlice.actions;
 
 export default userSlice.reducer;
