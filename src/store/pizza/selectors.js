34b0c5b29b32bc7595e067d07f5172b725201d24
hardcorebuddhist <im.energo@gmail.com>
2022-05-10 12:21:49 +0200

    first commit

---
+++ src/store/pizza/selectors.js
@@ -0,0 +1,12 @@
+export const selectNumberOfPizza = (reduxState) => {
+  return reduxState.pizzas.allPizzas.length;
+};
+
+export const selectMostBoughtPizza = (reduxState) => {
+  if (reduxState.pizzas.allPizzas.length === 0) {
+    return null;
+  }
+  return reduxState.pizzas.allPizzas.reduce((mostBought, nextPizza) => {
+    return mostBought.bought >= nextPizza.bought ? mostBought : nextPizza;
+  });
+};
---
+++ src/store/user/slice.js
@@ -0,0 +1,17 @@
+// src/store/user/slice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  name: "Helva",
+  id: 42,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+});
+
+export const {} = userSlice.actions;
+
+export default userSlice.reducer;
