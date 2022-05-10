40b8ff4d75193337dad5c034f0e07044282e278a
hardcorebuddhist <im.energo@gmail.com>
2022-05-10 13:40:38 +0200

    Ex3 sort by popularity

--- src/store/pizza/selectors.js
+++ src/store/pizza/selectors.js
@@ -11,4 +11,7 @@ export const selectMostBoughtPizza = (reduxState) => {
   });
 };
 
-export const selectPizzas = (reduxState) => reduxState.pizzas.allPizzas;
+export const selectPizzas = (reduxState) => {
+  const clonedArray = [...reduxState.pizzas.allPizzas];
+  return clonedArray.sort((a, b) => b.bought - a.bought);
+};
